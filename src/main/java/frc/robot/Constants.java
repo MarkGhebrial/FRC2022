a581538042685be7b9aff06fded30ab8eb54c3a7
MarkGhebrial <mghebrialjr@icloud.com>
2022-01-13 17:16:56 -0800

    Steal subsystem code from the 2021 robot

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,25 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public static class Drive {
+        /********** CAN ID's **********/
+        public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR_ID = 1;
+        public static final int FRONT_LEFT_MODULE_ROTATION_MOTOR_ID = 2;
+
+        public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID = 3;
+        public static final int FRONT_RIGHT_MODULE_ROTATION_MOTOR_ID = 4;
+
+        public static final int BACK_LEFT_MODULE_DRIVE_MOTOR_ID = 5;
+        public static final int BACK_LEFT_MODULE_ROTATION_MOTOR_ID = 6;
+
+        public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR_ID = 7;
+        public static final int BACK_RIGHT_MODULE_ROTATION_MOTOR_ID = 8;
+
+        /********** Motor Translations **********/
+        public static final Translation2d FRONT_LEFT_MODULE_TRANSLATION = new Translation2d(.5, .5);
+        public static final Translation2d FRONT_RIGHT_MODULE_TRANSLATION = new Translation2d(.5, -.5);
+        public static final Translation2d BACK_LEFT_MODULE_TRANSLATION = new Translation2d(-.5, .5);
+        public static final Translation2d BACK_RIGHT_MODULE_TRANSLATION = new Translation2d(-.5, -.5);
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,103 @@
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.SwerveModule3309;
+import friarLib2.utility.SwerveModule;
+
+import static frc.robot.Constants.Drive.*;
+
+public class DriveSubsystem extends SubsystemBase {
+
+    private SwerveModule frontLeftModule;
+    private SwerveModule frontRightModule;
+    private SwerveModule backLeftModule;
+    private SwerveModule backRightModule;
+
+    private SwerveDriveOdometry swerveOdometry;
+    private SwerveDriveKinematics swerveKinematics;
+    private Pose2d currentRobotPose = new Pose2d();
+
+    /**
+     * Initialize the swerve modules and Kinematics/Odometry objects
+     */
+    public DriveSubsystem() {
+        frontLeftModule = new SwerveModule3309(FRONT_LEFT_MODULE_DRIVE_MOTOR_ID, FRONT_LEFT_MODULE_ROTATION_MOTOR_ID, "Front left");
+        frontRightModule = new SwerveModule3309(FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID, FRONT_RIGHT_MODULE_ROTATION_MOTOR_ID, "Front right");
+        backLeftModule = new SwerveModule3309(BACK_LEFT_MODULE_DRIVE_MOTOR_ID, BACK_LEFT_MODULE_ROTATION_MOTOR_ID, "Back left");
+        backRightModule = new SwerveModule3309(BACK_RIGHT_MODULE_DRIVE_MOTOR_ID, BACK_RIGHT_MODULE_ROTATION_MOTOR_ID, "Back right");
+
+        swerveKinematics = new SwerveDriveKinematics(
+            FRONT_LEFT_MODULE_TRANSLATION,
+            FRONT_RIGHT_MODULE_TRANSLATION,
+            BACK_LEFT_MODULE_TRANSLATION,
+            BACK_RIGHT_MODULE_TRANSLATION
+        );
+
+        swerveOdometry = new SwerveDriveOdometry(swerveKinematics, getRobotRotation());
+    }
+
+    public void setModuleStates (SwerveModuleState[] states) {
+        frontLeftModule.setState(states[0]);
+        frontRightModule.setState(states[1]);
+        backLeftModule.setState(states[2]);
+        backRightModule.setState(states[3]);
+    }
+
+    /**
+     * Calculate and set the requred SwerveModuleStates for a given ChassisSpeeds
+     * 
+     * @param speeds
+     */
+    public void setChassisSpeeds (ChassisSpeeds speeds) {
+        SwerveModuleState[] moduleStates = swerveKinematics.toSwerveModuleStates(speeds); //Generate the swerve module states
+        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, SwerveModule3309.ABSOLUTE_MAX_DRIVE_SPEED);
+        setModuleStates(moduleStates);
+    }
+
+    /**
+     * Get the current robot pose according to dead-reckoning odometry
+     * See https://docs.wpilib.org/en/stable/docs/software/kinematics-and-odometry/swerve-drive-odometry.html for more details
+     * 
+     * @return Current robot pose
+     */
+    public Pose2d getRobotPose () {
+        return currentRobotPose;
+    }
+
+    /**
+     * Use the IMU to read the robot's yaw
+     * 
+     * @return Rotation2d representing IMU's measured angle
+     */
+    public Rotation2d getRobotRotation () {
+        double angle = /*imu.getAngle();*/ 0; // TODO: Add IMU code
+        return Rotation2d.fromDegrees(angle);
+    }
+
+    /**
+     * Set the odometry readings
+     * 
+     * @param pose Pose to be written to odometry
+     * @param rotation Roatation to be written to odometry
+     */
+    public void resetOdometry (Pose2d pose, Rotation2d rotation) {
+        swerveOdometry.resetPosition(pose, rotation);
+    }
+
+    @Override
+    public void periodic() {
+        //Update the odometry using module states and chassis rotation
+        currentRobotPose = swerveOdometry.update(
+            getRobotRotation(),
+            frontLeftModule.getState(),
+            frontRightModule.getState(),
+            backLeftModule.getState(),
+            backRightModule.getState()
+        );
+    }
+}
