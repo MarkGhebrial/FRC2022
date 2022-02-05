6832db68d990c28bfa4ad57e696fefabb39e25a1
MarkGhebrial <mghebrialjr@icloud.com>
2022-02-05 14:40:13 -0800

    Restructure IntakeSubsystem

--- src/main/java/frc/robot/commands/Intake.java
+++ src/main/java/frc/robot/commands/Intake.java
@@ -19,7 +19,7 @@ public class Intake extends CommandBase {
 
     @Override
     public void initialize() {
-        intake.deployBothIntakes();
+        intake.extendIntake();
     }
 
     @Override
@@ -29,7 +29,7 @@ public class Intake extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
-        intake.retractBothIntakes();
+        intake.retractIntake();
     }
 
     @Override
