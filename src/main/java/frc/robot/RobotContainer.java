e3f1e58ac7f3aa76c244cb9f8f58c689d40f1b44
MarkGhebrial <mghebrialjr@icloud.com>
2022-03-04 17:35:28 -0800

    Add bindings for the climber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.ExampleCommand;
@@ -22,6 +24,8 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.util.FiringSolution;
 import friarLib2.hid.LambdaTrigger;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -69,6 +73,14 @@ public class RobotContainer {
 
         new LambdaTrigger(() -> OI.operatorController.getLeftBumper() || OI.leftStickLeftCluster.get() || OI.leftStickRightCluster.get())
             .whileActiveContinuous(new IntakeAndIndex(intake, indexer));
+
+        // Extend the climber
+        new LambdaTrigger(() -> OI.operatorController.getRightBumper() && DriverStation.getMatchTime() <= 30)
+            .whenActive(new InstantCommand(climber::extendClimber, climber));
+
+        // Retract the climber
+        new LambdaTrigger(() -> OI.operatorController.getRightTriggerAxis() >= 0.5)
+            .whenActive(new InstantCommand(climber::retractClimber, climber));
 
         // Bind the oerator's D-pad to various shooting locations
         bindShootingCommand(Constants.Shooter.HIGH_HUB_FROM_FENDER, 0); // 0 is up, the values increase clockwise
