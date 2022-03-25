d4a7cba67a057324dc3ce28e5c73dcc8a496d919
MarkGhebrial <mghebrialjr@icloud.com>
2022-03-25 08:55:00 -0700

    Don't add a timeout if TimedCommand duration is zero

--- src/main/java/friarLib2/commands/TimedCommand.kt
+++ src/main/java/friarLib2/commands/TimedCommand.kt
@@ -6,13 +6,25 @@ import edu.wpi.first.wpilibj2.command.WaitCommand
 
 /**
  * A command that starts after a certain amount of time and ends after
- * some amount of time
+ * some amount of time.
+ *
+ * <p>If duration is zero, then don't add a timeout for the timed
+ * command, letting it run until it finishes by itself.
  */
 class TimedCommand(
     startTime: Double,
-    runTime: Double,
+    duration: Double,
     command: Command
-): SequentialCommandGroup(
-    WaitCommand(startTime),
-    command.withTimeout(runTime)
-)
+): SequentialCommandGroup() {
+    init {
+        if (startTime != 0.0) {
+            addCommands(WaitCommand(startTime))
+        }
+
+        if (duration != 0.0) {
+            addCommands(command.withTimeout(duration))
+        } else {
+            addCommands(command)
+        }
+    }
+}
