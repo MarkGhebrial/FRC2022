20d8f6086ef73e91437f76eed85b884fc1cbf1e4
MarkGhebrial <mghebrialjr@icloud.com>
2022-03-26 20:28:55 -0700

    Optimise TimedCommandBuilder

--- src/main/java/friarLib2/commands/builders/TimedCommandBuilder.kt
+++ src/main/java/friarLib2/commands/builders/TimedCommandBuilder.kt
@@ -2,11 +2,12 @@ package friarLib2.commands.builders
 
 import edu.wpi.first.wpilibj2.command.Command
 import friarLib2.commands.TimedCommand
+import java.util.Collections.emptyList
 
 class TimedCommandBuilder(
     var startTime: Double = 0.0,
     var runTime: Double = 0.0,
-    private val commandBuilder: ParallelCommandGroupBuilder = ParallelCommandGroupBuilder()
+    private val commands: MutableList<Command> = emptyList()
 ): CommandBuilder {
     var endTime: Double
         get() = startTime + runTime
@@ -15,9 +16,19 @@ class TimedCommandBuilder(
         }
 
     override operator fun Command.unaryPlus() {
-        commandBuilder.addCommands(this)
+        commands.add(this)
     }
 
-    override fun buildCommand(): Command =
-        TimedCommand(startTime, runTime, commandBuilder.buildCommand())
+    override fun buildCommand(): Command {
+        val command = group {
+            // Only create a parallel command group if we have more than one command to run
+            if (commands.size == 1) {
+                +commands[0]
+            } else {
+                +parallel { for (c in commands) { +c } }
+            }
+        }
+
+        return TimedCommand(startTime, runTime, command)
+    }
 }
