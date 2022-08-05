3699eb86c9814978ed6d23bfa0cc5fae86c4edb0
Mark Ghebrial <mghebrialjr@icloud.com>
2022-08-04 21:29:04 -0700

    Reduce climber sensitivity to joystick input

--- src/main/java/frc/robot/commands/climb/ClimbManual.kt
+++ src/main/java/frc/robot/commands/climb/ClimbManual.kt
@@ -3,6 +3,7 @@ package frc.robot.commands.climb
 import edu.wpi.first.wpilibj2.command.CommandBase
 import frc.robot.OI
 import frc.robot.subsystems.ClimberSubsystem
+import kotlin.math.pow
 
 class ClimbManual(private val climber: ClimberSubsystem) : CommandBase() {
     init {
@@ -11,7 +12,13 @@ class ClimbManual(private val climber: ClimberSubsystem) : CommandBase() {
 
     override fun execute() {
         if (climber.isExtended) {
-            climber.setClimberPower(OI.operatorController.leftYWithDeadband)
+            // Squaring the joystick value makes motor power output non-linearly
+            // related to joystick position, which should make low-speed adjustments
+            // easier for the operator
+            var motorPower = OI.operatorController.leftYWithDeadband.pow(2)
+            if (OI.operatorController.leftYWithDeadband < 0) { motorPower = -motorPower }
+
+            climber.setClimberPower(motorPower)
         } else {
             climber.setClimberPower(0.0)
         }
